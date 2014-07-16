104c9256652875134e8e4ee33dc7d7ebff19a572
Saurabh Asthana <rednblack@alum.mit.edu>
2014-07-16 14:54:10 -0700

    Automatic fasta loading from germ config

--- lib/fasta.rb
+++ lib/fasta.rb
@@ -1,9 +1,65 @@
 #!/usr/bin/env ruby
 require 'fasta_aux/fasta_aux'
+require 'germ/config'
 
 class Fasta
   private
   include FastaAux
+
+  class << self
+    GENOMES = {}
+    def guess_line_size file
+      io = File.open file
+
+      # Grab the first 50 lines
+      lines = 50.times.map do io.gets end.compact
+
+      chroms = lines.each_index.map.select do |i| lines[i] =~ /^>/ end
+
+      chroms.each_index do |i|
+        chrom_lines = lines[ chroms[i] + 1 ... chroms[i+1] || lines.size ]
+        # you need at least 2 lines to be sure.
+        next if chrom_lines.size < 2
+        return chrom_lines.first.chomp.size if chrom_lines[0].size == chrom_lines[1].size
+      end
+
+      # You can't guess, raise an exception.
+      raise "Could not guess file line size, please specify"
+    end
+
+    def default
+      sym = genome_file :default_genome
+      file = genome_file sym
+      load_genome sym, file if file && File.exists?(file)
+    end
+
+    def load_genome sym, file
+      GENOMES[sym] ||= Fasta.new file, genome_linesize(sym)
+    end
+
+    def method_missing sym, *args, &block
+      file = genome_file(sym)
+      return load_genome sym, file if file && file.is_a?(String) && File.exists?(file)
+      super sym, *args, &block
+    end
+
+    private
+    def genome_file sym
+      record = GermConfig.get_conf :fasta, sym
+      if record.is_a? Hash
+        record[:file] or raise "Config record is not well-formed, expected :file"
+      else
+        record
+      end
+    end
+
+    def genome_linesize sym
+      record = GermConfig.get_conf :fasta, sym
+      if record.is_a? Hash
+        record[:linesize] or raise "Config record is not well-formed, expected :linesize"
+      end
+    end
+  end
 
   class Chrom
     attr_reader :name, :size, :start
@@ -52,7 +108,7 @@ class Fasta
   def initialize file, size=nil
     @io = File.open(file)
 
-    @line_size = size || 50
+    @line_size = size || Fasta.guess_line_size(file)
 
     get_seq_starts
 
