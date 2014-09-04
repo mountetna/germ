4bb59ca896e19fb1e8cf78e970c1758d6d23c61d
Saurabh Asthana <rednblack@alum.mit.edu>
2014-09-04 15:34:05 -0700

    automatic defaults lookup for germ classes using Config file

--- lib/germ/config.rb
+++ lib/germ/config.rb
@@ -1,4 +1,6 @@
 require 'yaml'
+require 'extlib'
+
 class GermConfig
   def self.get_conf *keys
     @config ||= GermConfig.new
@@ -15,6 +17,7 @@ class GermConfig
 
   def get_key *keys
     keys.inject(@config) do |obj,key|
+      raise GermConfig::KeyError, "Broken key chain: #{keys}" unless obj
       obj = obj[key]
     end
   end
@@ -30,5 +33,61 @@ class GermConfig
 
   def load_file
     @config = YAML.load File.read(config_file)
+  end
+
+  class KeyError < StandardError
+  end
+end
+
+
+module GermDefault
+  CACHE = {}
+
+  def has_default *key_chain
+    @key_chain = key_chain
+  end
+
+  def default
+    # get the default key
+    CACHE[:default] ||= load_default
+  end
+
+
+  def cache_load key
+    CACHE[key] ||= load_key(key)
+  end
+
+  def method_missing sym, *args, &block
+    begin
+      cache_load sym
+    rescue GermConfig::KeyError
+      # the key does not exist, try to pass it on
+      super
+    end
+  end
+
+  protected
+  def default_create *args
+    args.each do |file|
+      raise "File not found: #{file}" unless File.exists?(file)
+    end
+    new *args
+  end
+
+  private
+  def key_chain
+    @key_chain ||= name.split(/::/).map do |name| name.snake_case.to_sym; end
+  end
+
+  def load_default
+    key = GermConfig.get_conf *key_chain, :default
+    raise GermConfig::KeyError, "No default key defined!" unless key
+    cache_load key
+  end
+
+  def load_key key
+    args = GermConfig.get_conf *key_chain, key
+    raise GermConfig::KeyError, "No such key defined!" unless args
+    default_create *args
   end
 end
