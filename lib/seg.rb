ecc4476199fbd8d5282b02cf9a1f7737b7f81a17
Saurabh Asthana <rednblack@alum.mit.edu>
2015-04-01 11:02:50 -0700

    seg update, making it use DNAcopy output by default

--- lib/seg.rb
+++ lib/seg.rb
@@ -1,27 +1,21 @@
 require 'hash_table'
 require 'genomic_locus'
 
-class SegFile < HashTable
+class Seg < HashTable
   include IntervalList
-  class SegHeader < HashTable::HashHeader
-    requires Sample: :str, Chromosome: :str, Start: :int, End: :int, Num_Probes: :int, Segment_Mean: :float
-    use_sleeve
-    replace_columns
-    print_header
-  end
-  class SegIndex < HashTable::HashIndex
-    always_index :sample
-  end
-  def initialize obj=nil, opts={}
-    opts = opts.merge(:skip_header => :true, :idx => :sample)
-    super obj, opts
-  end
-  class Segment < HashLine
+  columns :id, :chrom, :loc_start, :loc_end, :num_mark, :seg_mean
+  types chrom: :int, loc_start: :int, loc_end: :int, num_mark: :int, seg_mean: :float
+  display_names id: :ID, loc_start: :"loc.start", loc_end: :"loc.end", num_mark: :"num.mark", seg_mean: :"seg.mean"
+  index :id
+  parse_mode :orig
+  print_columns
+
+  class Segment < HashTable::Row
     include GenomicLocus
-    alias_key :seqname, :chromosome
+    alias_key :seqname, :chrom
     alias_key :stop, :end
     def copy
-      self.class.new @hash.clone, @table
+      default_copy
     end
   end
 end
---
+++ spec/seg_spec.rb
@@ -0,0 +1,15 @@
+require 'seg'
+
+describe Seg do
+  describe "#columns" do
+    context "loaded from file" do
+      it "parses correctly" do
+        m = Seg.new
+        m.parse "fixtures/seg.txt"
+
+        expect(m.columns).to eq [ :id, :chrom, :loc_start, :loc_end, :num_mark, :seg_mean ]
+        expect(m.display_names.values).to eq [ :ID, :"loc.start", :"loc.end", :"num.mark", :"seg.mean" ]
+      end
+    end
+  end
+end
