8a950ad3dc5c51424929c22e67b57d0ec96875da
Saurabh Asthana <rednblack@alum.mit.edu>
2015-04-06 12:11:37 -0700

    seg fixes

--- lib/seg.rb
+++ lib/seg.rb
@@ -4,7 +4,7 @@ require 'genomic_locus'
 class Seg < HashTable
   include IntervalList
   columns :id, :chrom, :loc_start, :loc_end, :num_mark, :seg_mean
-  types chrom: :int, loc_start: :int, loc_end: :int, num_mark: :int, seg_mean: :float
+  types loc_start: :int, loc_end: :int, num_mark: :int, seg_mean: :float
   display_names id: :ID, loc_start: :"loc.start", loc_end: :"loc.end", num_mark: :"num.mark", seg_mean: :"seg.mean"
   index :id
   parse_mode :orig
@@ -13,7 +13,8 @@ class Seg < HashTable
   class Segment < HashTable::Row
     include GenomicLocus
     alias_key :seqname, :chrom
-    alias_key :stop, :end
+    alias_key :start, :loc_start
+    alias_key :stop, :loc_end
     def copy
       default_copy
     end
